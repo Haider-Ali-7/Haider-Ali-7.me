a7070cf53b6d4c71a7596bb5b94377a50c0ba97f
Haider-Ali-7 <haideralitariqcheema@gmail.com>
2026-06-20 18:24:29 +0500

    - rm out - add dist

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export'
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
